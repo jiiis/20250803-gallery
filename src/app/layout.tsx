b5869545a37fa29f992f8c5a02d2d785d519183d
Hao Chang <yngwie.chang@gmail.com>
2025-08-18 00:35:14 +1000

    Add a gradient background

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,9 @@ export const metadata: Metadata = {
 const Layout = ({ children }: { children: ReactNode }) => (
   <html lang="en">
     <body className="m-0 p-0">
-      <main className="m-2">{children}</main>
+      <main className="min-h-screen w-screen bg-linear-to-br from-[#005c97] to-[#2e4db5] p-2">
+        {children}
+      </main>
     </body>
   </html>
 )
